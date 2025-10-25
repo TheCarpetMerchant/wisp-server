e7c173cc2d5f3e18b72750b0c9e19677c3d76589
TheCarpetMerchant <wgolyoko@gmail.com>
2025-10-25 15:18:08 +0200

    fix 1

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-import { Server } from '@mercuryworkshop/wisp-js/server';
+import { server as wisp } from '@mercuryworkshop/wisp-js/server';
 import { createServer } from 'http';
 
 const port = process.env.PORT || 8080;
-const server = createServer();
-const wisp = new Server({ server });
 
-server.listen(port, () => {
-  console.log(`Wisp server running on port ${port}`);
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Wisp server running');
 });
+
+server.on('upgrade', (req, socket, head) => {
+  wisp.routeRequest(req, socket, head);
+});
+
+server.on('listening', () => {
+  console.log(`Wisp server listening on port ${port}`);
+});
+
+server.listen(port, '0.0.0.0');
