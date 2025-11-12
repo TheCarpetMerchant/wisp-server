89c5be5809aa5657e4e4775dd62c5ecd4f605856
TheCarpetMerchant <wgolyoko@gmail.com>
2025-11-12 23:09:13 +0100

    change

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-import { server as wisp } from '@mercuryworkshop/wisp-js/server';
-import { createServer } from 'http';
+import { server as wisp } from "@mercuryworkshop/wisp-js/server";
+import http from "node:http";
 
-const port = process.env.PORT || 8080;
-
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Wisp server running');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("wisp server js rewrite");
 });
 
-server.on('upgrade', (req, socket, head) => {
+server.on("upgrade", (req, socket, head) => {
   wisp.routeRequest(req, socket, head);
 });
 
-server.on('listening', () => {
-  console.log(`Wisp server listening on port ${port}`);
+server.on("listening", () => {
+  console.log("HTTP server listening");
 });
 
-server.listen(port, '0.0.0.0');
+server.listen(5001, "127.0.0.1");
