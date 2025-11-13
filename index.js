60a8aba96ae9e0d9e8e990c5f4c185fdad291520
TheCarpetMerchant <wgolyoko@gmail.com>
2025-11-13 12:16:21 +0100

    port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { server as wisp } from "@mercuryworkshop/wisp-js/server";
 import http from "node:http";
 
+const port = process.env.PORT || 4000;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("wisp server js rewrite");
@@ -14,4 +16,4 @@ server.on("listening", () => {
   console.log("HTTP server listening");
 });
 
-server.listen(5001, "127.0.0.1");
+server.listen(port, "127.0.0.1");
